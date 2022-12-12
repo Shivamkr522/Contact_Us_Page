10b5481b9dd1db67aa1c9209140173992ecada8b
Shivamkr522 <105207334+Shivamkr522@users.noreply.github.com>
2022-12-12 12:55:47 +0530

    DashboardServlet updated

--- ContactUs/src/main/java/com/login/DashboardServlet.java
+++ ContactUs/src/main/java/com/login/DashboardServlet.java
@@ -1,7 +1,6 @@
 package com.login;
 import java.io.IOException;
 import java.util.*;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,35 +8,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import com.contactus.RequestDAO;
 import com.contactus.Request;
 @WebServlet("/dashboard")
 public class DashboardServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
-
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		List<Request> contactUsUserData = new ArrayList<>();
 		contactUsUserData = RequestDAO.fetchData();
 		HttpSession session=request.getSession();  
-        session.setAttribute("contactUsData",contactUsUserData);
-        
-        RequestDispatcher reqDis = request.getRequestDispatcher("dashboard.jsp");
-		reqDis.forward(request, response);
+        	session.setAttribute("contactUsData",contactUsUserData);
+        	RequestDispatcher reqDispatcher = request.getRequestDispatcher("dashboard.jsp");
+		reqDispatcher.forward(request, response);
 	}
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
-        String status = request.getParameter("status");
-        Request requestdata = new Request();
-        requestdata.setStatus(status);
-        requestdata.setId(id);
-        RequestDAO requestDao = new RequestDAO();
-        try {
-            requestDao.changeStatus(requestdata);
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        response.sendRedirect("dashboard");
+        	int id = Integer.parseInt(request.getParameter("id"));
+        	String status = request.getParameter("status");
+        	Request requestdata = new Request();
+        	requestdata.setStatus(status);
+        	requestdata.setId(id);
+        	RequestDAO requestDao = new RequestDAO();
+        	try {
+         		requestDao.changeStatus(requestdata);
+        	} 
+        	catch (Exception e) {
+        	    e.printStackTrace();
+        	}
+        	response.sendRedirect("dashboard");
 	}
 }
