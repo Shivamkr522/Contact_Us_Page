cee7e01b0fe5a2f32d83d30103e4a88db550e4ec
Shivamkr522 <105207334+Shivamkr522@users.noreply.github.com>
2022-12-12 12:57:26 +0530

    LoginDAO updated

--- ContactUs/src/main/java/com/login/LoginDAO.java
+++ ContactUs/src/main/java/com/login/LoginDAO.java
@@ -1,18 +1,15 @@
 package com.login;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
 public class LoginDAO {
 	public static final String DATABASE = "ContactUsDB";
-    public static final String HOST_NAME = "localhost";
-    public static final String HOST = "5432";
-    public static final String USER = "postgres";
-    public static final String PASSWORD = "postgres";
-    
-    public boolean isValidUser(Login usercheck) {
+    	public static final String HOST_NAME = "localhost";
+    	public static final String HOST = "5432";
+    	public static final String USER = "postgres";
+    	public static final String PASSWORD = "postgres";
+    	public boolean isValidUser(Login usercheck) {
     	try {
     		Class.forName("org.postgresql.Driver");
     		Connection con = DriverManager.getConnection("jdbc:postgresql://"+HOST_NAME+":"+HOST+"/"+DATABASE,USER,PASSWORD);
@@ -25,15 +22,11 @@ public class LoginDAO {
     			if(rt.getString(1)!=null&&rt.getString(2)!=null) {
     				return true;
     			}
-    			else
-    				break;
-    		}
-    		
+    		}	
     	}
     	catch(Exception e) {
     		e.printStackTrace();
-    	}
-    	
+	}
     	return false;
     }
 }
