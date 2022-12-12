9a409f9e2d437c6dc8d03188c44274fced3c9ed4
Shivamkr522 <105207334+Shivamkr522@users.noreply.github.com>
2022-12-12 12:53:47 +0530

    LogoutServlet updated

--- ContactUs/src/main/java/com/logout/LogoutServlet.java
+++ ContactUs/src/main/java/com/logout/LogoutServlet.java
@@ -1,13 +1,10 @@
 package com.logout;
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -17,5 +14,4 @@ public class LogoutServlet extends HttpServlet {
         session.invalidate();
         response.sendRedirect("login.jsp");
     }
-
 }
